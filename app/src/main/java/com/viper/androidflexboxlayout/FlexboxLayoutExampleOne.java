21ca056568ab4633aa6ba68cb4ca3795dd00899f
Suman Razz <viper.suman@gmail.com>
2017-12-19 16:48:21 +0545

    Initial commit

---
+++ app/src/main/java/com/viper/androidflexboxlayout/FlexboxLayoutExampleOne.java
@@ -0,0 +1,15 @@
+package com.viper.androidflexboxlayout;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.viralandroid.androidflexboxlayout.R;
+
+public class FlexboxLayoutExampleOne extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.flexbox_layout1);
+    }
+}
